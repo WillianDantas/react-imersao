f5668f923a4e84c536848c38c6bb1b2065e2a6e3
Willian Dantas <williansilva1991@gmail.com>
2020-08-03 17:56:32 -0300

    Criação da pagina de cadastro Video e categoria

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import  { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -7,10 +8,10 @@ import Button from '../Button';
 function Menu () {
    return(
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo"  src={Logo} alt="Logo" /> 
-            </a>
-            <Button className="ButtonLink" href="/">
+            </Link>
+            <Button  as={Link} className="ButtonLink" to="./Pages/Cadastro/Video">
                     Novo video  
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import  CadastroVideo from './Pages/Cadastro/Video';
+import CadastroCategoria from './Pages/Cadastro/Categoria';
+
+
+const Pagina404 = () => (<div>Página 404</div>)
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+        <Route path="/" component={App} exact />
+        <Route path="/Pages/Cadastro/Video" component={CadastroVideo} />
+        <Route path="/Pages/Cadastro/Categoria" component={CadastroCategoria} />
+        <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
